3b33bb9344381e2f9a1e25f6f558a813d8f74db9
Andres <andres.correa@designli.co>
2025-08-06 13:19:30 -0500

    fix: inject public metadata injection for webhook

--- libs/messaging-providers/src/receiver/mixins/message-receiver-controller.mixin.ts
+++ libs/messaging-providers/src/receiver/mixins/message-receiver-controller.mixin.ts
@@ -2,13 +2,19 @@
 import { Controller, Post, Req, NotFoundException } from '@nestjs/common';
 import { Request } from 'express';
 import { MessageRouterService } from '../services/message-router.service';
+import { Public } from '../decorators/public.decorator';
+import { PublicMetadata } from '../interfaces/message-receiver-options.interface';
 
-export function createMessageReceiverController(prefix: string): any {
+export function createMessageReceiverController(
+  prefix: string,
+  publicMetadata: PublicMetadata,
+): any {
   @Controller(prefix)
   class DynamicMessageReceiverController {
     constructor(private readonly router: MessageRouterService) {}
 
     @Post('*')
+    @Public(publicMetadata.key, publicMetadata.value)
     async handle(@Req() req: Request) {
       const path = req.path.replace(`/${prefix}`, '');
       const handler = this.router.getHandlerForPath(path || '/');
